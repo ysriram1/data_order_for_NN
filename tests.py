9a598fbf421d224de8ab92d2fa59630fa4a795da
Sriram Yarlagadda <sriram@Srirams-MacBook-Pro.local>
2018-04-25 23:07:29 -0500

    fixed errors + inc epochs

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.optimizers import RMSprop
@@ -15,16 +16,18 @@ import os
 def gen_confusion_mat(y_real, y_pred, pprint=True):
     y_pred_indices = []
     y_real_indices = []
-    for arr in range(len(y_pred)):
-        y_pred_indices.append(y_pred.argmax())
-        y_real_indices.append(y_real.argmax())
+    for i in range(len(y_pred)):
+        y_pred_indices.append(y_pred[i].argmax())
+        y_real_indices.append(y_real[i].argmax())
     confusion_mat = confusion_matrix(y_real_indices, y_pred_indices)
     if pprint: print(confusion_mat)
     return confusion_mat
 
 # returns a flat vector instead of an image matrix. Also converts image to gray
 def flatten_img(img):
-    gray = cv2.rgb2gray(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = gray.astype('float32')
+    gray = gray/255
     return gray.flatten()
 
 # TODO: add flags
@@ -32,28 +35,30 @@ flags = tf.app.flags
 
 # load data
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('before flattening...')
 print('Input shape: ', x_train.shape)
 num_classes = 10
 y_train, y_test = to_categorical(y_train, num_classes), to_categorical(y_test, num_classes)
-# basic preprocessing
-x_train, x_test = x_train.astype('float32'), x_test.astype('float32')
-x_train, x_test = x_train/255, x_test/255
+# generate grayscale flattened arrays
+x_train = np.array([flatten_img(img) for img in x_train])
+x_test = np.array([flatten_img(img) for img in x_test])
+print('after flattening...')
+print('Input shape: ', x_train.shape)
 
 # the parameters
 BATCH = 800
-EPOCHS = 10
+EPOCHS = 30
 LEARN = 0.001 # learning rate
 
 # the network (feedforward without convolutional layers)
 model = Sequential()
-model.add(Dense(100, input_shape=(32,32,3)))
+model.add(Dense(256, input_shape=(1024,)))
 model.add(Activation('relu'))
-model.add(Flatten())
-model.add(Dense(200))
+model.add(Dense(128))
 model.add(Activation('relu'))
-model.add(Dense(200))
+model.add(Dense(64))
 model.add(Activation('relu'))
-model.add(Dense(200))
+model.add(Dense(32))
 model.add(Activation('relu'))
 model.add(Dense(10))
 model.add(Activation('softmax'))
@@ -79,4 +84,5 @@ model.fit(x_train, y_train,
             shuffle=True)
 
 predictions =  model.predict(x_test)
+print(predictions)
 gen_confusion_mat(y_test, predictions)
